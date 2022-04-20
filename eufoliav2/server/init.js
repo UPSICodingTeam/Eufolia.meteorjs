53c178e91532ab879d5c9e0a9a0b0b9531560a4f
Hafiz Hanif <apitlekays@gmail.com>
2022-04-20 16:04:42 +0800

    redacted codes

--- eufoliav2/server/init.js
+++ eufoliav2/server/init.js
@@ -2,5 +2,5 @@ import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
 //tuts on setting up email in meteor using mailgun.org: https://themeteorchef.com/tutorials/using-the-email-package
-  process.env.MAIL_URL = 'smtp://postmaster%40sandbox8322fda008c8485193f2b65e3ff1e703.mailgun.org:22ed13254dca23b95eb3aae09de4008c@smtp.mailgun.org:587';
+  process.env.MAIL_URL = '/****REDACTED CODE****/';
 });
